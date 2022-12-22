bc3e8234dc8e62a5272b23782ea2db7b816f2741
Arun Tiwari <69424940+itiwariarun@users.noreply.github.com>
2022-12-22 11:11:07 +0530

    Update ContactForm.jsx

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -4,7 +4,7 @@ const ContactForm = () => {
   return (
     <div className="w-full flex flex-col justify-center">
       <div className="leading-loose">
-        <form
+        <form name="contact" method="POST" data-netlify="true"
           onSubmit={(e) => {
             e.preventDefault();
 
@@ -28,6 +28,7 @@ const ContactForm = () => {
               name="name"
               type="text"
               required
+value="name" 
               placeholder="Your Name"
               aria-label="Name"
             />
@@ -44,6 +45,7 @@ const ContactForm = () => {
               id="email"
               name="email"
               type="text"
+value="email" 
               required
               placeholder="Your Email"
               aria-label="Email"
@@ -62,6 +64,7 @@ const ContactForm = () => {
               name="subject"
               type="text"
               required
+value="subject" 
               placeholder="Subject"
               aria-label="Subject"
             />
@@ -78,6 +81,7 @@ const ContactForm = () => {
               className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-slate-700 dark:text-secondary-light bg-slate-300 dark:bg-ternary-dark rounded-md shadow-sm text-md"
               id="message"
               name="message"
+value="message" 
               cols="14"
               rows="6"
               aria-label="Message"
